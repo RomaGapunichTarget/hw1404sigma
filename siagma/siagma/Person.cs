9d5ce4d72d20284936e71714e98695dd9c77a470
RomaGapunich <romagapunich@gmail.com>
2019-04-15 00:09:49 +0300

    добавил чет

---
+++ siagma/siagma/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace siagma
+{
+    public class Persons
+    {
+        public string Name { get; set; }
+
+        public string Surname
+        {
+            get ;
+            set;
+        }
+
+        public Persons(string name,string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+    }
+
+    public class PeopleWorked : IEnumerable<Persons>,IEnumerator<Persons>
+    {
+        public Persons Current => throw new NotImplementedException();
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<Persons> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- siagma/siagma/Program.cs
+++ siagma/siagma/Program.cs
@@ -1,12 +1,78 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace siagma
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            WorkHasSetCollection(); // It's HashSetCoolectionWork
+           
+
+            Console.ReadKey();
+
         }
+
+        #region HashCollection
+        private static void WorkHasSetCollection()
+        {
+            HashSet<int> evenNumbers = new HashSet<int>();
+            HashSet<int> oddNumbers = new HashSet<int>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    // Populate numbers with just even numbers.
+                    evenNumbers.Add(i);
+                }
+                else
+                {
+                    // Populate oddNumbers with just odd numbers.
+                    oddNumbers.Add((i));
+                }
+
+            }
+
+            Console.Write("evenNumbers contains {0} elements: ", evenNumbers.Count);
+            DisplaySet(evenNumbers);
+
+            Console.Write("oddNumbers contains {0} elements: ", oddNumbers.Count);
+            DisplaySet(oddNumbers);
+
+            Remove(evenNumbers);
+            Remove(oddNumbers);
+
+            Console.Write("evenNumbers contains {0} elements after divide: ", evenNumbers.Count);
+            DisplaySet(evenNumbers);
+
+            Console.Write("oddNumbers contains {0} elements after divide: ", oddNumbers.Count);
+            DisplaySet(oddNumbers);
+        }
+
+        private static void DisplaySet(HashSet<int> set)
+        {
+            Console.Write("{");
+            foreach (int i in set)
+            {
+                Console.Write(" {0}", i);
+            }
+            Console.WriteLine(" }");
+        }
+
+        private static void Remove(HashSet<int> set)
+        {
+            foreach (int i in set)
+            {
+                if (i/5>0)
+                {
+                    set.RemoveWhere(i1 => i1/5>0);
+                    break;
+                }
+                
+            }
+        }
+        #endregion
     }
 }
