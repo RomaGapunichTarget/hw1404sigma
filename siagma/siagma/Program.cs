2a7556470998782971162fa39ff00a689d56b893
RomaGapunich <romagapunich@gmail.com>
2019-04-14 16:57:11 +0300

    добавлен проект

---
+++ siagma/siagma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace siagma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
